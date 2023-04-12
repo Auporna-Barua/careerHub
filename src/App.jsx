eea55d032cbac150aee051f5bc85c8b381d14c59
Auporna-Barua <120823697+Auporna-Barua@users.noreply.github.com>
2023-04-12 22:37:20 +0600

    getwork website hero section complate with responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './components/header/Header'
-import Footer from './components/footer/Footer'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Hero from "./components/hero/Hero";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <Header />
+      <Hero />
       <Footer />
     </>
   );
 }
 
-export default App
+export default App;
