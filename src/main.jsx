ef7aec8dbf94a725de377d2f68f0829aa633fb1f
Auporna-Barua <120823697+Auporna-Barua@users.noreply.github.com>
2023-04-12 22:24:21 +0600

    all router add with react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,34 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-
+import { createBrowserRouter } from 'react-router-dom';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/blog",
+    element: <Blog />,
+  },
+  {
+    path: "/Statistics",
+    element: <Statistic />,
+  },
+  {
+    path: "/applied",
+    element: <Applied />,
+  },
+  {
+    path: "/details/:id",
+    element: <Details />,
+    loader: () => fetch("data.json"),
+  },
+  {
+    path: "*",
+    element: <Error />,
+  },
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
